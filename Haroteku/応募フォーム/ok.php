5958da21365c93c0f2a772d36b6bb53e0ba67f8e
yuuu016 <MiyakeYuta08@outlook.jp>
2025-12-13 22:03:37 +0900

    Add files via upload

---
+++ Haroteku/応募フォーム/ok.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8">
+ <link rel="stylesheet" href="cs.css">
+<title>登録完了</title> 
+</head>
+
+<body>
+
+<br/>
+    <form method="post" action="Controller.php">
+      <p>登録完了しました。</p><br/>
+      <p>ありがとうございました</p><br/>
+
+</body>
+
+</html>
